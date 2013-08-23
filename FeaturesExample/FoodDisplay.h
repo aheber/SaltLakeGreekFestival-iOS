ea0dcb4e7536041deab9c938389d4640cd0505cc
Brad Littler <glittler@gmail.com>
2013-08-23 00:05:15 -0600

    Food Display addition

---
+++ FeaturesExample/FoodDisplay.h
@@ -0,0 +1,13 @@
+//
+//  FoodDisplay.h
+//  SLCGreekFestival
+//
+//  Created by Brad Littler on 8/22/13.
+//  Copyright (c) 2013 Adriaenssen BVBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodDisplay : UIView
+
+@end
---
+++ FeaturesExample/FoodDisplay.m
@@ -0,0 +1,31 @@
+//
+//  FoodDisplay.m
+//  SLCGreekFestival
+//
+//  Created by Brad Littler on 8/22/13.
+//  Copyright (c) 2013 Adriaenssen BVBA. All rights reserved.
+//
+
+#import "FoodDisplay.h"
+
+@implementation FoodDisplay
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
